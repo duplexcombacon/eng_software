a4ffbac6a2081f41c48181ca06a2b5ff5958c658
Martim182004 <martim.goncalves.silva@ubi.pt>
2025-11-11 22:42:59 +0000

    ultima versao atualizada
    ultima versao atualizada

--- server/db.js
+++ server/db.js
@@ -3,33 +3,16 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// Lógica NOVA (condicional)
-
-// Configuração base
 const dbConfig = {
-    server: process.env.DB_SERVER,
-    database: process.env.DB_NAME,
-    options: {
-        encrypt: false // Mude para true se estiver a usar Azure, etc.
-    },
-    pool: { // Exemplo se estiver a usar pooling
-        max: 10,
-        min: 0,
-        idleTimeoutMillis: 30000
-    }
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  server: process.env.DB_SERVER,
+  database: process.env.DB_NAME,
+  options: {
+    encrypt: false,
+    trustServerCertificate: true
+  }
 };
-
-// Adiciona autenticação SQL apenas se DB_USER existir no .env
-if (process.env.DB_USER) {
-    dbConfig.user = process.env.DB_USER;
-    dbConfig.password = process.env.DB_PASS;
-} else {
-    // Caso contrário, usa a Autenticação do Windows
-    dbConfig.options.trustedConnection = true;
-}
-
-// Agora use 'dbConfig' para criar a sua ligação
-// ex: await sql.connect(dbConfig)
 
 export const poolPromise = new sql.ConnectionPool(dbConfig)
   .connect()
